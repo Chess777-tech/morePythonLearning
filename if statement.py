85f767aa0febd7d672768c880754f23e4a7d0376
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-06-11 23:19:12 -0500

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,18 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = int(input("Please enter an integer: "))
+Please enter an integer: 47
+>>> if x < 0:
+...     x = 0
+...     print("Negative changed to zero")
+... elif x == 0:
+...     print("zero")
+... elif x == 1:
+...     print("single")
+... else:
+...     print("More")
+... 
+...     
+More
+>>> 
+>>> 
