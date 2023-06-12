5e456be1ffaa0abb6d4000bfff809e34e68b8fe1
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-06-11 23:23:27 -0500

    Add files via upload

---
+++ for statement.py
@@ -0,0 +1,10 @@
+"""
+ Pythons for statement iterates over the items of any sequence (a list or a string), 
+ in the order that they appear in the sequence
+
+"""
+
+words = ['cat:' ,'window:','defenestrate:']
+for w in words:
+    print(w, len(w))
+    
